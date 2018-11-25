fdd8c2379909c4001ad72d0e5d8e9ad3d94f69e3
DavidRandisi <david_randisi@hotmail.com>
2018-11-25 16:46:45 +0100

    Created FallBackCollision script

---
+++ FluffyInvention/Assets/Scripts/FallBackCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallBackCollision : MonoBehaviour {
+    private Rigidbody myRigidbody;
+
+    void Start()
+    {
+        this.myRigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision entered");
+        myRigidbody.AddForce(-transform.forward * 100);
+        myRigidbody.useGravity = true;
+    }
+}
