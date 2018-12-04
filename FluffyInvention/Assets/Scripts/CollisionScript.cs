db92796e01a54e6fa036fb208b150d1cdde4e20c
DavidRandisi <david_randisi@hotmail.com>
2018-12-04 20:22:18 +0100

    Added smoke particle on tree collision (To be tested on Vive)

---
+++ FluffyInvention/Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+    public ParticleSystem SmokeParticle;
+
+    // Use this for initialization
+    void Start()
+    {
+        SmokeParticle.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Tree")
+        {
+            SmokeParticle.Play();
+        }
+    }
+}
